5b790e346e1fcbdcb45fb6014bc9820f04ce94cc
Débora Paulin <debipaulin@gmail.com>
2023-04-30 16:44:35 -0300

    ajuste de js

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,11 @@
-var numeroSecreto = parseInt(Math.random() * 11);
+let numeroEscondido = parseInt(Math.random() * 11);
 
 function Chutar() {
-  var elementoResultado = document.getElementById("resultado");
-  var chute = parseInt(document.getElementById("valor").value);
+  let elementoResultado = document.getElementById("resultado");
+  let chute = parseInt(document.getElementById("valor").value);
 
   console.log(chute);
-  if (chute == numeroSecreto) {
+  if (chute == numeroEscondido) {
     elementoResultado.innerHTML = "Parabéns! Você acertou";
   } else if (chute > 10 || chute < 0) {
     elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10";
